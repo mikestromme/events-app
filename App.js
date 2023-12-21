74e9a4c3a64540f325e34790be04036b30560a30
Mike Stromme <mikestromme@gmail.com>
2023-12-21 06:19:44 -0600

    x

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Platform, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontSize:32}}>Hello World!!!</Text>
+      <Text style={{fontSize:16}}>Platforn : {Platform.OS == 'ios' ? 'ios' : 'android'}</Text>
+      <ScrollView>
+        <View style={{backgroundColor: 'red', height: 1600}}>
+          <Text>
+            Hello There Mike!!
+          </Text>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +22,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
